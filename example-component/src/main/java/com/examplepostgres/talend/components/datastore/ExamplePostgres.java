5e9d41307bf218bcdfb7a9acf3c4b048aeb1487d
Ramyadeep-Dey <73230596+Ramyadeep-Dey@users.noreply.github.com>
2020-10-23 21:05:25 +0530

    added new code

--- example-component/src/main/java/com/examplepostgres/talend/components/datastore/ExamplePostgres.java
+++ example-component/src/main/java/com/examplepostgres/talend/components/datastore/ExamplePostgres.java
@@ -1,6 +1,12 @@
 package com.examplepostgres.talend.components.datastore;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.action.Checkable;
@@ -8,6 +14,7 @@ import org.talend.sdk.component.api.configuration.type.DataStore;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.configuration.ui.widget.Credential;
 import org.talend.sdk.component.api.meta.Documentation;
+import org.talend.sdk.component.api.service.completion.SuggestionValues;
 
 @DataStore("ExamplePostgres")
 @GridLayout({
@@ -86,5 +93,31 @@ public class ExamplePostgres implements Serializable {
     public ExamplePostgres setDatabase(String database) {
         this.database = database;
         return this;
+    }
+
+    public Connection getConnection() throws Exception
+    {
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:postgresql://"+this.getUrl()+":"+this.getPort()+"/"+this.getDatabase(),this.getUsername(),this.getPassword());
+        return conn;
+    }
+
+    public List<SuggestionValues.Item> getTables() {
+        List<SuggestionValues.Item> tables = new ArrayList<SuggestionValues.Item>();
+        try{
+            Connection conn = this.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select concat(schemaname,'.',tablename) from pg_catalog.pg_tables where tableowner = '"+this.getDatabase()+"' order by schemaname, tablename");
+            while(rs.next())
+            {
+                tables.add(new SuggestionValues.Item(rs.getString(1),rs.getString(1)));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return tables;
     }
 }
--- example-component/src/main/java/com/examplepostgres/talend/components/service/ExampleComponentService.java
+++ example-component/src/main/java/com/examplepostgres/talend/components/service/ExampleComponentService.java
@@ -6,6 +6,8 @@ import com.examplepostgres.talend.components.datastore.ExamplePostgres;
 
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.service.Service;
+import org.talend.sdk.component.api.service.completion.SuggestionValues;
+import org.talend.sdk.component.api.service.completion.Suggestions;
 import org.talend.sdk.component.api.service.healthcheck.HealthCheck;
 import org.talend.sdk.component.api.service.healthcheck.HealthCheckStatus;
 
@@ -17,13 +19,20 @@ public class ExampleComponentService {
     @HealthCheck(value = "validateConnection")
     public HealthCheckStatus healthCheck(@Option ExamplePostgres conn) {
         try{
-            conn.getConection();
+            conn.getConnection();
             return new HealthCheckStatus(HealthCheckStatus.Status.OK, "Success");
         } catch(Exception e){
             return new HealthCheckStatus(HealthCheckStatus.Status.KO, "Failed >" + e.getMessage());
         }
             
     }
-    
 
+    @Suggestions("showTables")
+    public SuggestionValues listTables(@Option ExamplePostgres datastore)
+    {
+        SuggestionValues values = new SuggestionValues();
+        values.setItems(datastore.getTables());
+
+        return values;
+    }
 }
